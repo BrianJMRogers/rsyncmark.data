52fac5a4fcf71650cf69a75caba44d34642edd72
Brian Rogers <Brian@Brians-MacBook-Pro.local>
2018-03-24 17:48:25 -0400

    pushing example file

---
+++ R/throughput.R
@@ -0,0 +1,16 @@
+#' Print hello
+#'
+#' This function prints hello
+#'
+#' @param fname First name
+#' @param lname Last name
+#' @export
+#' @examples
+#' hello(fname="Tomas",lname="Greif")
+#' hello(fname="Your",lname="Name")
+
+hello <- function(fname, lname) {
+  cat(paste("Hello",fname,lname,"!"))
+}
+
+hello("Brian","Rogers")
