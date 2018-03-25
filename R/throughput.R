90fdfd893940547e74298bec0d378541808604e7
Brian Rogers <Brian@Brians-MacBook-Pro.local>
2018-03-25 15:02:29 -0400

    turing em all into functions

--- R/throughput.R
+++ R/throughput.R
@@ -3,25 +3,40 @@
 library(tidyverse)
 library(dplyr)
 
-# read data files
-data_full <- read_csv("data/data.csv")
-
-View(data_full)
+read_data <- function()
+{
+  # read data files
+  data_full <- read_csv("data/data.csv")
+  return data_full
+}
 
 # break table into large/medium and small/tiny
-large_medium <- filter(data_full, file_size_name != "small", file_size_name != "tiny")
-small_tiny <- filter(data_full, file_size_name != "large", file_size_name != "medium")
 
-#### THROUGHPUT ####
-# graph violin plot
-ggplot(data = data_full, mapping = aes(x = trial_name, y = throughput_bytes_per_second)) + geom_violin() + facet_wrap(~ file_size_name)
 
-# graph line graph
-ggplot(data = data_full, mapping = aes(x = trial_num, y = throughput_bytes_per_second, color = trial_name)) + geom_jitter() + facet_wrap(~ file_size_name)
+violin_plot_throughput <- function()
+{
+  data_full <- read_data()
+  # graph line graph
+  ggplot(data = data_full, mapping = aes(x = trial_num, y = throughput_bytes_per_second, color = trial_name)) + geom_jitter() + facet_wrap(~ file_size_name)
+}
 
-#### REAL_TIME_SECONDS ####
+point_plot_throughput <- function()
+{
+  data_full <- read_data()
+  # graph line graph
+  ggplot(data = data_full, mapping = aes(x = trial_num, y = throughput_bytes_per_second, color = trial_name)) + geom_jitter() + facet_wrap(~ file_size_name)
+}
 
-# large_medium
-ggplot(data = large_medium, mapping = aes(x = trial_num, y = real_time_seconds, color = trial_name)) + geom_jitter() + facet_wrap(~ file_size_name)
-ggplot(data = small_tiny, mapping = aes(x = trial_num, y = real_time_seconds, color = trial_name)) + geom_jitter() + facet_wrap(~ file_size_name)
-mean(data_full$real_time_seconds)
+graph_large_medium_real_time <- function()
+{
+  data_full <- read_data()
+  large_medium <- filter(data_full, file_size_name != "small", file_size_name != "tiny")
+  ggplot(data = large_medium, mapping = aes(x = trial_num, y = real_time_seconds, color = trial_name)) + geom_jitter() + facet_wrap(~ file_size_name)
+}
+
+graph_small_tiny_real_time <- function()
+{
+  data_full <- read_data()
+  small_tiny <- filter(data_full, file_size_name != "large", file_size_name != "medium")
+  ggplot(data = small_tiny, mapping = aes(x = trial_num, y = real_time_seconds, color = trial_name)) + geom_jitter() + facet_wrap(~ file_size_name)
+}
