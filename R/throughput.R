4195e82268f578b57d7da6925fd20bf2b1858e8d
Brian Rogers <Brian@Brians-MacBook-Pro.local>
2018-03-24 23:10:34 -0400

    finished writing graphing code

--- R/throughput.R
+++ R/throughput.R
@@ -1,16 +1,27 @@
-#' Print hello
-#'
-#' This function prints hello
-#'
-#' @param fname First name
-#' @param lname Last name
-#' @export
-#' @examples
-#' hello(fname="Tomas",lname="Greif")
-#' hello(fname="Your",lname="Name")
+# this file was written by Brian Rogers
 
-hello <- function(fname, lname) {
-  cat(paste("Hello",fname,lname,"!"))
-}
+library(tidyverse)
+library(dplyr)
 
-hello("Brian","Rogers")
+# read data files
+data_full <- read_csv("data/data.csv")
+
+View(data_full)
+
+# break table into large/medium and small/tiny
+large_medium <- filter(data_full, file_size_name != "small", file_size_name != "tiny")
+small_tiny <- filter(data_full, file_size_name != "large", file_size_name != "medium")
+
+#### THROUGHPUT ####
+# graph violin plot
+ggplot(data = data_full, mapping = aes(x = trial_name, y = throughput_bytes_per_second)) + geom_violin() + facet_wrap(~ file_size_name)
+
+# graph line graph
+ggplot(data = data_full, mapping = aes(x = trial_num, y = throughput_bytes_per_second, color = trial_name)) + geom_jitter() + facet_wrap(~ file_size_name)
+
+#### REAL_TIME_SECONDS ####
+
+# large_medium
+ggplot(data = large_medium, mapping = aes(x = trial_num, y = real_time_seconds, color = trial_name)) + geom_jitter() + facet_wrap(~ file_size_name)
+ggplot(data = small_tiny, mapping = aes(x = trial_num, y = real_time_seconds, color = trial_name)) + geom_jitter() + facet_wrap(~ file_size_name)
+mean(data_full$real_time_seconds)
