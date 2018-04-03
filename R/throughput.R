6eb6c465450ecdd4ba1e39be38860737d445a84b
Brian Rogers <Brian@Brians-MacBook-Pro.local>
2018-04-02 23:12:24 -0400

    updated to add shapes to graphs

--- R/throughput.R
+++ R/throughput.R
@@ -21,7 +21,7 @@ point_plot_throughput <- function()
 {
   data_full <- read_data()
   # graph line graph
-  ggplot(data = data_full, mapping = aes(x = trial_num, y = throughput_bytes_per_second, color = trial_name)) + geom_jitter() + facet_wrap(~ file_size_name)
+  ggplot(data = data_full, mapping = aes(x = trial_num, y = throughput_bytes_per_second, color = trial_name, shape = trial_name)) + geom_jitter() + facet_wrap(~ file_size_name)
 }
 
 
@@ -30,12 +30,12 @@ graph_large_medium_real_time <- function()
 {
   data_full <- read_data()
   large_medium <- filter(data_full, file_size_name != "small", file_size_name != "tiny")
-  ggplot(data = large_medium, mapping = aes(x = trial_num, y = real_time_seconds, color = trial_name)) + geom_jitter() + facet_wrap(~ file_size_name)
+  ggplot(data = large_medium, mapping = aes(x = trial_num, y = real_time_seconds, color = trial_name, shape = trial_name)) + geom_jitter() + facet_wrap(~ file_size_name)
 }
 
 graph_small_tiny_real_time <- function()
 {
   data_full <- read_data()
   small_tiny <- filter(data_full, file_size_name != "large", file_size_name != "medium")
-  ggplot(data = small_tiny, mapping = aes(x = trial_num, y = real_time_seconds, color = trial_name)) + geom_jitter() + facet_wrap(~ file_size_name)
+  ggplot(data = small_tiny, mapping = aes(x = trial_num, y = real_time_seconds, color = trial_name, shape = trial_name)) + geom_jitter() + facet_wrap(~ file_size_name)
 }
